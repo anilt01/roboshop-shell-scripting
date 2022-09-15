18e5c19120268c0551dcc4f3c6dd9cc50438dac4
anilt01 <tanil010398@gmail.com>
2022-09-15 19:02:44 -0400

    added status command

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,23 @@
  echo Installing Nginx
  yum install nginx -y &>> /tmp/frontend.sh
+ echo status =?
 
  echo Downloading Nginx web content
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> /tmp/frontend.sh
+ echo status =?
 
  echo switch directory
  cd /usr/share/nginx/html &>> /tmp/frontend.sh
 
  echo remove previous web content
  rm -rf * &>> /tmp/frontend.sh
-
+echo status =?
  echo unzipping files
  unzip /tmp/frontend.zip &>> /tmp/frontend.sh
  mv frontend-main/static/* . &>> /tmp/frontend.sh
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/frontend.sh
-
+echo status =?
  echo Starting Nginx Service
  systemctl enable nginx &>> /tmp/frontend.sh
  systemctl restart nginx &>> /tmp/frontend.sh
+ echo status =?
