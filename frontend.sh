7ce948fe3f70d248cb529a3bc76cae2ba5393afa
anilt01 <tanil010398@gmail.com>
2022-09-14 20:15:40 -0400

    added redirectories to move the unwanted data to tmp files

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
  echo Installing Nginx
- yum install nginx -y
+ yum install nginx -y &>> /tmp/frontend.sh
 
  echo Downloading Nginx web content
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> /tmp/frontend.sh
 
  echo switch directory
- cd /usr/share/nginx/html
+ cd /usr/share/nginx/html &>> /tmp/frontend.sh
 
  echo remove previous web content
- rm -rf *
+ rm -rf * &>> /tmp/frontend.sh
 
  echo unzipping files
- unzip /tmp/frontend.zip
- mv frontend-main/static/* .
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+ unzip /tmp/frontend.zip &>> /tmp/frontend.sh
+ mv frontend-main/static/* . &>> /tmp/frontend.sh
+ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/frontend.sh
 
  echo Starting Nginx Service
- systemctl enable nginx
- systemctl restart nginx
+ systemctl enable nginx &>> /tmp/frontend.sh
+ systemctl restart nginx &>> /tmp/frontend.sh
