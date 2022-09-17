da8b86ff443a197c800942b36379da33d5265b21
anilt01 <tanil010398@gmail.com>
2022-09-17 17:08:55 -0400

    recommit

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,24 @@
  LOG_FILE=/tmp/frntend
  echo Installing Nginx
- yum install nginx -y &>>/tmp/frntend
- echo status=$?
+ yum install nginx -y &>>LOG_FILE
+echo status=$?
 
  echo Downloading Nginx web content
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> /tmp/frontend
  echo status=$?
 
  echo switch directory
- cd /usr/share/nginx/html &>>/tmp/frntend
+ cd /usr/share/nginx/html &>>LOG_FILE
 
  echo remove previous web content
- rm -rf * &>>/tmp/frntend
+ rm -rf * &>>LOG_FILE
 echo status=$?
  echo unzipping files
- unzip /tmp/frontend.zip &>>/tmp/frntend
- mv frontend-main/static/* . &>>/tmp/frntend
+ unzip /tmp/frontend.zip &>>LOG_FILE
+ mv frontend-main/static/* . &>>LOG_FILE
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/frontend
 echo status=$?
  echo Starting Nginx Service
- systemctl enable nginx &>>/tmp/frntend
- systemctl restart nginx &>>/tmp/frntend
+ systemctl enable nginx &>>LOG_FILE
+ systemctl restart nginx &>>LOG_FILE
  echo status=$?
