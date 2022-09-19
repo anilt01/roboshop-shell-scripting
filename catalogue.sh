36ac029bf82060502be580afc7f57a1d05a8ce0d
anilt01 <tanil010398@gmail.com>
2022-09-18 23:50:26 -0400

    updated files with common file

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ source common.sh
 
 echo "Download NodeJS package"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-statuscheck $?
+StatusCheck $?
 
 echo "Installing NodeJS package"
 yum install nodejs -y &>>${LOG_FILE}
@@ -13,41 +13,41 @@ id roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
 echo "Adding roboshop user to the application"
 useradd roboshop &>>${LOG_FILE}
-echo status=$?
+StatusCheck $?
 fi
 
 echo "Downloading application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-echo status=$?
+StatusCheck $?
 
 cd /home/roboshop
 
 echo " clear previous files/ app data"
 rm -rf catalogue catalogue-main &>>${LOG_FILE}
-echo status=$?
+StatusCheck $?
 
 echo "extracting application code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
-echo status=$?
+StatusCheck $?
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "Install NodeJs Dependencies"
 npm install &>>${LOG_FILE}
-echo status=$?
+StatusCheck $?
 
 echo "Updating Mongodb Ip address in SystemD file"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
-echo status=$?
+StatusCheck $?
 
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 
 echo "catalogue service setup"
 systemctl daemon-reload &>>${LOG_FILE}
 systemctl enable catalogue &>>${LOG_FILE}
-echo status=$?
+StatusCheck $?
 
 echo "restarting catalogue service"
 systemctl start catalogue &>>${LOG_FILE}
-echo status=$?
+StatusCheck $?
--- common.sh
+++ common.sh
@@ -1,4 +1,9 @@
-statuscheck() {
+ID=$ (id -u)
+if [ id -ne 0 ]; then
+  echo "you should run this script as a root user or with sudo privileges"
+fi
+
+StatusCheck() {
 if [ $? -eq 0 ];
 then
 echo -e status= "\e[32mSUCCESS\e[0m"
