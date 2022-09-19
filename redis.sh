3c20cac277b3a2d6affd91a28375d2b9f8fcf70d
anilt01 <tanil010398@gmail.com>
2022-09-19 19:29:04 -0400

    Setting up redis

--- redis.sh
+++ redis.sh
@@ -1,23 +1,24 @@
 source common.sh
+LOG_FILE=/tmp/redis
 
 echo "Downloading redis remi package"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-Statuscheck $?
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG_FILE}
+StatusCheck $?
 
 echo "enabling redis version 6.2"
-dnf module enable redis:remi-6.2 -y
-Statuscheck $?
+dnf module enable redis:remi-6.2 -y &>>${LOG_FILE}
+StatusCheck $?
 
 echo "Installing redis package"
-yum install redis -y
-Statuscheck $?
+yum install redis -y &>>${LOG_FILE}
+StatusCheck $?
 
 echo "updating Listen address in config files"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
-Statuscheck $?
-## Update the `bind` from 127.0.0.1 to 0.0.0.0` in config file /etc/redis.conf` & `/etc/redis/redis.conf`
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG_FILE}
+StatusCheck $?
+## Update the `bind` from 127.0.0.1 to 0.0.0.0` in config file /etc/redis.conf` & `/etc/redis/redis.conf` &>>${LOG_FILE}
 
 echo "enable and restart redis"
-systemctl enable redis
-systemctl restart redis
-Statuscheck $?
+systemctl enable redis &>>${LOG_FILE}
+systemctl restart redis &>>${LOG_FILE}
+StatusCheck $?
