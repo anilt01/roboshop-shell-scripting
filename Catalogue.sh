07225e41c5e92e43d84166a26953de6e28d4813f
anilt01 <tanil010398@gmail.com>
2022-09-18 20:13:34 -0400

    Setting catalogue application

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,44 +1,39 @@
+
 LOG_FILE=/tmp/catalogue
 
-echo "Download NodeJS package"
+echo "Downloading NodeJS package"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-echo Status=$?
+echo status=$?
 
-echo "Installing NodeJs"
+echo "Installing NodeJs package"
 yum install nodejs -y &>>${LOG_FILE}
 echo status=$?
 
-echo "Adding roboshop user"
+echo "Adding User roboshop in the application"
 useradd roboshop &>>${LOG_FILE}
 echo status=$?
 
-echo "Download Catalogue Application code"
+echo "downloading catalogue application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 echo status=$?
 
 cd /home/roboshop
 
-echo "extracting catalogue application code"
+echo "extracting the catalogue application code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
 echo status=$?
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
-echo "Install NodeJS dependencies"
+echo "Installing catalogue application code"
 npm install &>>${LOG_FILE}
 echo status=$?
 
-echo "update systemd files with mongodb address"
-sed -i -e 's/MONGO_DNSNAME/mogodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
-echo status=$?
-
-echo "setup catalogue service"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-echo status=$?
-
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload &>>${LOG_FILE}
 systemctl enable catalogue &>>${LOG_FILE}
-echo "restart catalogue service"
+
+echo "restarting catalogue service"
 systemctl restart catalogue &>>${LOG_FILE}
 echo status=$?
