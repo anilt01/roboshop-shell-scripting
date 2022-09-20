290818bf806dc75bdbe701a15ad12698f69313a7
anilt01 <tanil010398@gmail.com>
2022-09-20 16:31:18 -0400

    Added code for creating Mysql Server

---
+++ mysql.sh
@@ -0,0 +1,35 @@
+LOG_FILE=/tmp/mysql
+source common.sh
+
+echo "Downloading mysql repo file"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
+StatusCheck $?
+
+echo "disabling previous mysql module"
+dnf module disable mysql -y &>>${LOG_FILE}
+StatusCheck $?
+
+echo "Installing the required community mysql server"
+yum install mysql-community-server -y &>>${LOG_FILE}
+StatusCheck $?
+
+echo "enable mysql server"
+systemctl enable mysqld &>>${LOG_FILE}
+StatusCheck $?
+
+echo "restarting the server"
+systemctl start mysqld
+StatusCheck $?
+
+grep temp /var/log/mysqld.log
+
+
+# mysql_secure_installation
+# mysql -uroot -pRoboShop@1
+## IN MY SQL>
+# uninstall plugin validate_password;
+# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+# cd /tmp
+# unzip mysql.zip
+# cd mysql-main
+# mysql -u root -pRoboShop@1 <shipping.sql
